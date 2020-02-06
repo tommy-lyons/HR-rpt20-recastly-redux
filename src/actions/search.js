9ff79e5dbf63b79c5d0f3138437bb461c41badaf
Justin Anderson <justand@gmail.com>
2020-02-05 19:35:33 -0800

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,16 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+
+  // return a thunk (function) which will call searchYoutube with the search field query
+  return function() {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      q: q
+    };
+    searchYouTube(options, changeVideoList);
+  }
 };
 
 export default handleVideoSearch;
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -3,5 +3,5 @@ import Redux from 'redux';
 var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
 };
-
+// test comment
 export default currentVideoReducer;
