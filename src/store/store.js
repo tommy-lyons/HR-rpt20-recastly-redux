ba22edc55d467afd9e8e24b72edd52726cc6ecf6
Justin Anderson <justand@gmail.com>
2020-02-07 10:30:07 -0800

    Complete connecting combine reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -8,15 +8,20 @@ import exampleVideoData from '../data/exampleVideoData.js';
 // search query
 const initialState = {
   videoList: exampleVideoData,
-  currentVideo: exampleVideoData[0],
-  searchQuery: ''
+  currentVideo: null
+  // searchQuery: ''
 }
+console.log(rootReducer);
+var store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
-// is this always the same, is this the default?
-export default function configureStore(initialState) {
-  return createStore(
-      rootReducer,
-      initialState,
-      applyMiddleware(thunk)
-  );
-}
+export default store;
+// ^^ this version now passes STORE tests and does not break test page
+
+// CODE BELOW DOES NOT PASS STORE TESTS BUT ALSO DOES NOT BREAK TEST PAGE
+// export default function configureStore(initialState) {
+//   return createStore(
+//       rootReducer,
+//       initialState,
+//       applyMiddleware(thunk)
+//   );
+// }
